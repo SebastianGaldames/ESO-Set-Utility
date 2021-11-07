d891e6a5c23331a52e562568372cd3ce96e1ffd8
shisah <47121081+Shisah@users.noreply.github.com>
2021-11-07 15:19:21 -0300

    [npm install necesario en server, agregado axios] agregada validacion con un secret basico

--- server/controllers/scrapperController.js
+++ server/controllers/scrapperController.js
@@ -1,14 +1,18 @@
 const axios = require('axios')
-const secret = '4atmBrPlHQ'
+const scrapperService = require('../services/scrapperService')
 
-function test() {
-  return 'hello im the scrapper'
+const test = async (req, res) => {
+  res.send(scrapperService.testService())
 }
 
 const scrap = async (req, res) => {
-  const url = req.body.url
-  const response = await axios.get(url)
-  res.send(response.data)
+  if (!scrapperService.auth(req.body.secret)) {
+    res.status(401).send('secret invalid')
+  } else {
+    const url = scrapperService.url
+    const response = await axios.get(url)
+    res.send(response.data)
+  }
 }
 
-module.exports = { scrap }
+module.exports = { scrap, test }
--- server/routes/scrapper.js
+++ server/routes/scrapper.js
@@ -2,17 +2,11 @@ const express = require('express')
 const scrapperController = require('../controllers/scrapperController')
 const router = express.Router()
 
-var options = {
-  host: 'www.google.com',
-}
-
 router.get('/', (req, res) => {
   res.send({ msg: 'scrapper route test' })
 })
 
-router.get('/test', (req, res) => {
-  res.send(scrapperController.test())
-})
+router.get('/test', scrapperController.test)
 
 router.post('/scrap', scrapperController.scrap)
 
