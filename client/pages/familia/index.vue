ccf62a353e7b76541c376cf2828c67593281ae5f
Mecho-sys <nrojazamorano@hotmail.com>
2021-11-08 20:53:55 -0300

    Creacion de galeria de items de la familia

--- client/pages/familia/index.vue
+++ client/pages/familia/index.vue
@@ -5,45 +5,54 @@
         <v-toolbar-title align="center">Aqui estaria la barra</v-toolbar-title>
       </v-toolbar>
     </div>
-    <div class="w-50">
+    <div c lass="w-50">
       <v-row>
         <v-col class="mx-auto" md="6" sm="8">
           <v-container class="grey lighten-5">
             <v-row no-gutters>
-              <v-col cols="12" sm="4">
-                <h1 class="title text-center lista">Set:</h1>
+              <v-col sm="2"></v-col>
+              <v-col sm="6">
+                <h1 class="title text-center lista">
+                  Set: "Nombre de la Familia"
+                </h1>
               </v-col>
               <v-spacer />
             </v-row>
             <v-row>
-              <v-col class="lista" md="3">
+              <v-col class="der" md="3">
                 <h3>Obtencion:</h3>
               </v-col>
               <v-col>
-                <h3 class="info">infoPertinente</h3>
+                <p class="izp">infoPertinente</p>
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="lista" md="3">
+              <v-col class="der" md="3">
                 <h3>DLC requerido:</h3>
               </v-col>
-            </v-row>
-            <v-row>
-              <v-col class="lista" md="3">
-                <h3>Estilo:</h3>
+              <v-col>
+                <p class="izp">infoPertinente</p>
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="lista" md="3">
+              <v-col class="der" md="3">
+                <h3>Estilo:</h3>
+              </v-col>
+              <v-col>
+                <p class="izp">infoPertinente</p>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="der" md="3">
                 <h3>Localización:</h3>
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="lista" md="3">
+              <v-col class="der" md="3">
                 <h3></h3>
               </v-col>
               <v-col>
-                <h3>Ubicacion</h3>
+                <p>Ubicacion</p>
               </v-col>
             </v-row>
           </v-container>
@@ -56,11 +65,11 @@
                   <v-row>
                     <v-col md="1"></v-col>
                     <v-col md="4">
-                      <h5>obtención</h5>
+                      <h5>"obtención"</h5>
                     </v-col>
                     <v-col md="1"></v-col>
                     <v-col md="5">
-                      <h5 class="lista">level 50 - CP 160</h5>
+                      <h5 class="der">level "50" - CP "160"</h5>
                     </v-col>
                     <v-col md="1"></v-col>
                   </v-row>
@@ -77,6 +86,13 @@
                     </v-col>
                     <v-col md="5"></v-col>
                   </v-row>
+                  <v-row>
+                    <v-col md="3"></v-col>
+                    <v-col md="6">
+                      <h1 class="centro">"Nombre de la Familia"</h1>
+                    </v-col>
+                    <v-col md="3"></v-col>
+                  </v-row>
                   <v-spacer />
                   <v-row>
                     <v-col md="3"></v-col>
@@ -89,11 +105,11 @@
                   <v-spacer />
                   <v-row>
                     <v-col md="3"></v-col>
-                    <v-col class="buffs">
-                      <p>(2 items) A</p>
-                      <p>(3 items) A</p>
-                      <p>(4 items) A</p>
-                      <p>(5 items) A</p>
+                    <v-col class="centro">
+                      <p>(2 items) "buffo de estadisticas"</p>
+                      <p>(3 items) "buffo de estadisticas"</p>
+                      <p>(4 items) "buffo de estadisticas"</p>
+                      <p>(5 items) "bono por uso de la familia"</p>
                     </v-col>
                     <v-col md="3"></v-col>
                   </v-row>
@@ -105,19 +121,49 @@
         <v-col class="mx-auto" md="2"></v-col>
       </v-row>
     </div>
-    <div></div>
+    <v-spacer />
+    <div class="w-100">
+      <v-row class="galery">
+        <v-col v-for="n in 20" :key="n" md="2">
+          <v-card v-ripple class="item" outlined @click="redirigir()">
+            <img
+              class="test"
+              src="https://eso-hub.com/storage/icons/gear_breton_heavy_head_d.webp"
+              alt=""
+            />
+            <h5 class="centro">"Tipo</h5>
+            <h4 class="centro">nombre</h4>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </section>
 </template>
 
 <style>
-.lista {
+.test {
+  display: block;
+  margin: 0 auto;
+}
+.item {
+  max-width: 150px;
+  max-height: 150px;
+}
+.w-100 {
+  width: 100%;
+}
+.galery {
+  width: 75%;
+  margin: 0 auto;
+}
+.der {
   justify-content: right;
   text-align: right;
 }
-.info {
+.izq {
   text-align: left;
 }
-.buffs {
+.centro {
   text-align: center;
 }
 </style>
