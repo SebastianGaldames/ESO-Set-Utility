7730b7d0b5dd7f7f9ec23d768279e53bfb4025dc
Tyrael1999 <jamch1999@gmail.com>
2021-11-08 15:08:28 -0300

    Creada page Perfil junto a sus componentes Cuenta, Inventario y Personajes

---
+++ client/components/Cuenta.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    <v-text-field label="Usuario" />
+    <v-text-field label="Pais" />
+    <v-text-field label="Sexo" />
+    <v-text-field label="Correo" />
+    <v-text-field label="Contraseña actual" />
+    <v-text-field label="Nueva contraseña" />
+    <v-text-field label="Confirmar nueva contraseña" />
+  </v-card>
+</template>
