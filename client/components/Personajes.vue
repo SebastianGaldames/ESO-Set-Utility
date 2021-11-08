0224471bc5911402151975a3d296990751d0603f
Mrldmz <leopoldo.munoz.m@gmail.com>
2021-11-08 17:10:25 -0300

    lista estatica temporal

---
+++ client/components/Personajes.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-list>
+      <v-subheader>Personajes</v-subheader>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>nombre de personaje</v-list-item-title>
+          <v-list-item-subtitle>info por definir</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>nombre de personaje</v-list-item-title>
+          <v-list-item-subtitle>info por definir</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>nombre de personaje</v-list-item-title>
+          <v-list-item-subtitle>info por definir</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
