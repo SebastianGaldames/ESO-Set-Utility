37a14988563ce70d9e4cba58c0b0d55801afa012
Mrldmz <leopoldo.munoz.m@gmail.com>
2021-11-08 17:52:02 -0300

    creado componente PersonajeCard para usar en lista dinamica

---
+++ client/components/PersonajeCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-list-item two-line>
+    <v-list-item-content>
+      <v-list-item-title>nombre de personaje</v-list-item-title>
+      <v-list-item-subtitle>info por definir</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
