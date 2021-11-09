3e294253bb23d2a4f66dfa29f5aa09fb159f0dd0
ChristianLizama <ckill661@gmail.com>
2021-11-09 16:10:29 -0300

    Testeando obtener datos de base de datos

---
+++ client/pages/familysearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <tabla-items></tabla-items>
+    <!-- <label for="">{{ post }}</label> -->
+    <label for="">{{ post }}</label>
+  </div>
+</template>
+
+<script>
+import TablaItems from '~/components/Buscador/TablaItems.vue'
+export default {
+  components: { TablaItems },
+  // async asyncData({ params, $http }) {
+  //   const post = await $http.$get(
+  //     `http://localhost:9000/Familia/list${params.id}`
+  //   )
+  //   return { post }
+  // },
+}
+</script>
