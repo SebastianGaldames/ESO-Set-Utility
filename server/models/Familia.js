86fa7b585d6f7e0d9bb815c6a76aca90cbe874ac
Diego Aguilera <diegoaguilera4@icloud.com>
2021-11-08 21:18:41 -0300

    Cambios en modelos familia e item

--- server/models/Familia.js
+++ server/models/Familia.js
@@ -1,10 +1,17 @@
 const { Schema, model } = require('mongoose')
 
+
+const bonoSchema = new Schema({
+  texto: { type: String },
+  cantidad: { type: Number },
+  estadisticas: { type: [String] }
+})
+
 const familiaSchema = new Schema({
   nombre: { type: String, required: true },
   ubicacion: { type: [String], required: true },
   itemsFamilia: { type: [Schema.ObjectId], ref: 'Item'},
-  bonificacion: { type: [String], required: true }
+  bonos: {type: [bonoSchema], required: true }
 })
 
 module.exports = model('Familia', familiaSchema)
