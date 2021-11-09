d4ae53d47b6aa4ad9abcc64bbc17fda66e833b83
Francisco-q <quevedof98@gmail.com>
2021-11-08 23:27:54 -0300

    captura datos login y registro + logica v1

--- client/pages/Login/index.vue
+++ client/pages/Login/index.vue
@@ -1,64 +1,112 @@
 <template>
-  <v-layout justify-center>
-    <v-flex xs12 sm10 md8 lg6>
-      <v-card>
-        <v-flex xs12 sm6>
-          <v-text-field
-            v-model="mensaje"
-            label="Usuario"
-            hint="Ingresa tu Usuario"
-            clearable
-          ></v-text-field>
-        </v-flex>
+  <form @submit.prevent="buscarPerfil">
+    <v-layout justify-center>
+      <v-flex xs12 sm10 md8 lg6>
+        <v-card>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="nombreUsuario"
+              input
+              type="nombreUsuario"
+              label="Usuario"
+              hint="Ingresa tu Usuario"
+              clearable
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex xs12 sm6>
-          <v-text-field
-            :rules="[rules.required, rules.min]"
-            :type="show2 ? 'text' : 'password'"
-            name="input-10-2"
-            label="Contraseña"
-            hint="Ingresa tu contraseña"
-            value="wqfasds"
-            class="input-group--focused"
-            @click:append="show2 = !show2"
-          ></v-text-field>
-        </v-flex>
-        <v-col>
-          <v-row>
-            <div>
-              <v-btn flat small>¿Olvidaste tu contraseña?</v-btn>
-            </div>
-          </v-row>
-          <v-row>
-            <div class="text-xs-center">
-              <v-btn round color="error" dark>Iniciar Sesión</v-btn>
-            </div>
-          </v-row>
-          <v-row>
-            <div>
-              ¿No tienes cuenta?
-              <v-btn flat small>Regístrate</v-btn>
-            </div>
-          </v-row>
-        </v-col>
-      </v-card>
-    </v-flex>
-  </v-layout>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="password"
+              :rules="[rules.required, rules.min]"
+              :type="show2 ? 'text' : 'password'"
+              name="password"
+              label="Contraseña"
+              hint="Ingresa tu contraseña"
+              value="wqfasds"
+              class="input-group--focused"
+              @click:append="show2 = !show2"
+            ></v-text-field>
+          </v-flex>
+          <v-col>
+            <v-row>
+              <div>
+                <v-btn text small>¿Olvidaste tu contraseña?</v-btn>
+              </div>
+            </v-row>
+            <v-row>
+              <div class="text-xs-center">
+                <v-btn type="buscar-perfil" rounded color="error" dark
+                  >Iniciar Sesión</v-btn
+                >
+              </div>
+            </v-row>
+            <v-row>
+              <div>
+                ¿No tienes cuenta?
+                <v-btn text small>Regístrate</v-btn>
+              </div>
+            </v-row>
+          </v-col>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </form>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
-      show: false,
-      password: 'Password',
+      show2: false,
+      password: '',
+      nombreUsuario: '',
+      errorM: '',
+
       rules: {
         required: (value) => !!value || 'Campo obligatorio',
         min: (v) => v.length >= 8 || 'Debe tener al menos 8 caracteres',
       },
     }
+  },
+  methods: {
+    /**
+     * @author Francisco Quevedo
+     * Busca si existe un usuario en la base de datos con los parametros tomados del front
+     * si lo encuentra guarda un token que contiene el rol, nombre usuario y id
+     */
+    buscarPerfil() {
+      axios
+        .post('http://localhost:4000/api/Usuario/login', {
+          nombreUsuario: this.nombreUsuario,
+          password: this.password,
+        })
+        .then((respuesta) => {
+          return respuesta.data
+        })
+        .then((data) => {
+          this.$store.dispatch('guardarToken', data.tokenReturn)
+          console.log(data.tokenReturn)
+          if (this.$store.state.usuario.rol === 'admin') {
+            this.$router.push('/') // push a admin??
+          } else {
+            this.$router.push('/') // push a cuenta de usuario pendiente
+          }
+        })
+        .catch((error) => {
+          this.errorM = null
+          if (error.response.status === 404) {
+            this.errorM =
+              'No existe el usuario o las condiciones son incorrectas'
+            console.log(this.errorM)
+          } else {
+            this.errorM = 'Ocurrio un error con el servidor'
+          }
+          console.log(error)
+        })
+    },
   },
 }
 </script>
--- client/pages/Registro/index.vue
+++ client/pages/Registro/index.vue
@@ -1,101 +1,162 @@
 <template>
-  <v-layout justify-center>
-    <v-flex xs12 sm10 md8 lg6>
-      <v-card>
-        <v-autocomplete
-          ref="pais"
-          v-model="pais"
-          :rules="[() => !!pais || '¡Este campo es obligatorio!']"
-          :items="paises"
-          label="Pais"
-          placeholder="Selecciona..."
-          required
-        ></v-autocomplete>
+  <form @submit.prevent="registroNuevoUsuario">
+    <v-layout justify-center>
+      <v-flex xs12 sm10 md8 lg6>
+        <v-card>
+          <v-autocomplete
+            ref="país"
+            v-model="pais"
+            :rules="[() => !!pais || '¡Este campo es obligatorio!']"
+            :items="paises"
+            label="País"
+            placeholder="Selecciona..."
+            required
+          ></v-autocomplete>
 
-        <v-flex xs12 sm6>
-          <v-text-field
-            v-model="mensaje"
-            label="Ingresa tu ID"
-            clearable
-          ></v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <v-text-field
-            v-model="mensaje"
-            label="Email"
-            clearable
-          ></v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <v-text-field
-            v-model="mensaje"
-            label="Confirma tu email"
-            clearable
-          ></v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <v-text-field
-            :rules="[rules.required, rules.min]"
-            :type="show2 ? 'text' : 'password'"
-            name="input-10-2"
-            label="Ingresa tu contraseña"
-            hint="Debe tener al menos 8 caracteres"
-            value="wqfasds"
-            class="input-group--focused"
-            @click:append="show2 = !show2"
-          ></v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <v-text-field
-            :rules="[rules.required, rules.min]"
-            :type="show2 ? 'text' : 'password'"
-            name="input-10-2"
-            label="Confirma tu contraseña"
-            hint="Debe coincidir con la contraseña escrita en el campo anterior"
-            value="wqfasds"
-            class="input-group--focused"
-            @click:append="show2 = !show2"
-          ></v-text-field>
-        </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="idUsuario"
+              label="Ingresa tu ID"
+              clearable
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="email"
+              label="Email"
+              clearable
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="reEmail"
+              :rules="[rules.required, rules.min, emailConfirmationRule]"
+              label="Confirma tu email"
+              hint="Debe coincidir con el email anterior escrito en el campo anterior"
+              clearable
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="password"
+              :rules="[rules.required, rules.min]"
+              :type="show2 ? 'text' : 'password'"
+              name="input-10-2"
+              label="Ingresa tu contraseña"
+              hint="Debe tener al menos 8 caracteres"
+              value="wqfasds"
+              class="input-group--focused"
+              @click:append="show2 = !show2"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="rePassword"
+              :rules="[rules.required, rules.min, passwordConfirmationRule]"
+              :type="show2 ? 'text' : 'password'"
+              name="input-10-2"
+              label="Confirma tu contraseña"
+              hint="Debe coincidir con la contraseña escrita en el campo anterior"
+              value="wqfasds"
+              class="input-group--focused"
+              @click:append="show2 = !show2"
+            ></v-text-field>
+          </v-flex>
 
-        <v-checkbox
-          v-model="checkbox"
-          :error-messages="checkboxErrors"
-          label="Estoy de acuerdo con los términos y condiciones"
-          required
-          @change="$v.checkbox.$touch()"
-          @blur="$v.checkbox.$touch()"
-        ></v-checkbox>
-        <v-row>
-          <v-col>
-            <div class="text-xs-center">
-              <v-btn round color="error" dark>Registrarme</v-btn>
-            </div>
-          </v-col>
-          <v-col>
-            <div class="text-xs-center">
-              <v-btn round dark>cancelar</v-btn>
-            </div>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-flex>
-  </v-layout>
+          <v-checkbox
+            :error-messages="checkboxErrors"
+            label="Estoy de acuerdo con los términos y condiciones"
+            required
+            @change="$v.checkbox.$touch()"
+            @blur="$v.checkbox.$touch()"
+          ></v-checkbox>
+          <v-row>
+            <v-col>
+              <div class="text-xs-center">
+                <v-btn round color="error" dark>Registrarme</v-btn>
+              </div>
+            </v-col>
+            <v-col>
+              <div class="text-xs-center">
+                <v-btn round dark>cancelar</v-btn>
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </form>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios'
+
+class Usuario {
+  constructor(rol, idUsuario, email, pais, password) {
+    this.rol = rol
+    this.idUsuario = idUsuario
+    this.email = email
+    this.pais = pais
+    this.password = password
+  }
+}
 export default {
   data() {
     return {
-      show: false,
-      password: 'Password',
+      rol: 'usuario',
+      idUsuario: '',
+      show2: false,
+      pais: '',
+      password: '',
+      rePassword: '',
+      email: '',
+      reEmail: '',
+      usuario: new Usuario(),
+
       rules: {
         required: (value) => !!value || 'Campo obligatorio',
         min: (v) => v.length >= 8 || 'Debe tener al menos 8 caracteres',
       },
     }
+  },
+  computed: {
+    passwordConfirmationRule() {
+      return () =>
+        this.password === this.rePassword || 'Las contraseñas no coinciden'
+    },
+    emailConfirmationRule() {
+      return () => this.email === this.ReEmail || 'Los email no coinciden'
+    },
+  },
+  methods: {
+    registroNuevoUsuario() {
+      this.usuario = new Usuario(
+        this.rol,
+        this.idUsuario,
+        this.email,
+        this.pais,
+        this.password
+      )
+      axios
+        .post('http://localhost:4000/api/Usuario/add', {
+          rol: 'usuario',
+          idUsuario: this.usuario.idUsuario,
+          email: this.usuario.email,
+          pais: this.usuario.pais,
+          password: this.usuario.password,
+        })
+        .then((respuesta) => {
+          return respuesta.data
+        })
+        .then((data) => {
+          this.$router.push('/')
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
   },
 }
 </script>
