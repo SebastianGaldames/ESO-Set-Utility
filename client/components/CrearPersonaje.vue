74e7055ff6b2d0f34949e3b70f96d29ce7475789
Joaquin S <joaq4699@hotmail.com>
2021-11-08 21:46:18 -0300

    Formulario para creación de personaje, faltan verificaciones

---
+++ client/components/CrearPersonaje.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card max-width="400">
+    <v-card-title primary-title> Crear Personaje </v-card-title>
+    <v-card-text>
+      <v-container grid-list-xs>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="newPersonaje.nombre"
+              label="Nombre"
+              clearable
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text @click="cerrarEvent()">Cancelar</v-btn>
+      <v-btn text @click="postPersonaje()">Crear</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newPersonaje: {
+        nombre: '',
+      },
+      formHasErrors: false,
+    }
+  },
+  computed: {
+    form() {
+      return {
+        nombre: this.newPersonaje.nombre,
+      }
+    },
+  },
+  methods: {
+    clearData() {
+      this.newPersonaje.nombre = ''
+    },
+    cerrarEvent() {
+      this.clearData()
+      this.$emit('cerrarDialog', false)
+    },
+    postPersonaje() {
+      //   const newPersonaje = {
+      //     nombre: this.newPersonaje.nombre,
+      //   }
+      this.cerrarEvent()
+    },
+  },
+}
+</script>
