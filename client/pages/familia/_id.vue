09d4c01d5d8259927bd370feb56b612a7d887f97
Mecho-sys <nrojazamorano@hotmail.com>
2021-11-09 18:46:28 -0300

    Se aplico el tema para la pagina dentro de esta vista

--- client/pages/familia/_id.vue
+++ client/pages/familia/_id.vue
@@ -1,48 +1,51 @@
 <template>
-  <section>
+  <section color="primario">
     <div class="w-100 mx-auto">
-      <v-toolbar color="primary">
-        <v-toolbar-title align="center">Aqui estaria la barra</v-toolbar-title>
+      <v-toolbar color="primario">
+        <v-toolbar-title align="center" class="secundario--text">
+          Aqui estaria la barra
+        </v-toolbar-title>
       </v-toolbar>
     </div>
-    <div class="w-100">
+    <hr class="secundario" />
+    <div class="w-100 primario">
       <v-row class="galery">
-        <v-col class="mx-auto" md="6" sm="8">
-          <v-container class="grey lighten-5">
+        <v-col class="mx-auto" color="primario" md="6" sm="8">
+          <v-container class="primario">
             <v-row no-gutters>
               <v-col sm="6">
-                <h1 class="title text-center lista">
-                  Set: {{ $route.params.item.nombre }}
+                <h1 class="title text-center lista texto1--text">
+                  Set: "Nombre de la Familia"
                 </h1>
               </v-col>
               <v-spacer />
             </v-row>
             <v-row>
-              <v-col class="der" md="3">
+              <v-col class="der texto1--text" md="3">
                 <h3>Obtencion:</h3>
               </v-col>
               <v-col>
-                <p class="izp">infoPertinente</p>
+                <p class="izp secundario--text">infoPertinente</p>
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="der" md="3">
+              <v-col class="der texto1--text" md="3">
                 <h3>DLC requerido:</h3>
               </v-col>
               <v-col>
-                <p class="izp">infoPertinente</p>
+                <p class="izp secundario--text">infoPertinente</p>
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="der" md="3">
+              <v-col class="der texto1--text" md="3">
                 <h3>Estilo:</h3>
               </v-col>
               <v-col>
-                <p class="izp">infoPertinente</p>
+                <p class="izp secundario--text">infoPertinente</p>
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="der" md="3">
+              <v-col class="der texto1--text" md="3">
                 <h3>Localización:</h3>
               </v-col>
               <v-col></v-col>
@@ -52,23 +55,23 @@
                 <h3></h3>
               </v-col>
               <v-col>
-                <p>Ubicacion</p>
+                <p class="secundario--text">Ubicacion</p>
               </v-col>
             </v-row>
           </v-container>
         </v-col>
-        <v-col class="mx-auto" md="6" sm="8">
-          <v-container class="grey lighten-5">
+        <v-col class="mx-auto" md="6" sm="8" color="primario">
+          <v-container>
             <v-row no-gutters>
               <v-col>
-                <v-card class="pa-2" outlined tile>
+                <v-card class="pa-2 borde" color="primario" outlined tile>
                   <v-row class="galery">
                     <v-col md="4">
-                      <h5>"obtención"</h5>
+                      <h5 class="texto1--text">"obtención"</h5>
                     </v-col>
 
                     <v-col md="8">
-                      <h5 class="der">level "50" - CP "160"</h5>
+                      <h5 class="der texto1--text">level "50" - CP "160"</h5>
                     </v-col>
                   </v-row>
                   <v-spacer />
@@ -84,7 +87,9 @@
                   <v-row>
                     <v-col md="3"></v-col>
                     <v-col md="6">
-                      <h1 class="centro">{{ $route.params.item.nombre }}</h1>
+                      <h1 class="centro texto1--text">
+                        "Nombre de la Familia"
+                      </h1>
                     </v-col>
                     <v-col md="3"></v-col>
                   </v-row>
@@ -92,14 +97,14 @@
                   <v-row>
                     <v-col md="3"></v-col>
                     <v-col md="6">
-                      <hr />
+                      <hr class="secundario" />
                     </v-col>
                     <v-col md="3"></v-col>
                   </v-row>
 
                   <v-spacer />
                   <v-row>
-                    <v-col class="centro">
+                    <v-col class="centro texto1--text">
                       <p>(2 items) "buffo de estadisticas"</p>
                       <p>(3 items) "buffo de estadisticas"</p>
                       <p>(4 items) "buffo de estadisticas"</p>
@@ -114,17 +119,23 @@
       </v-row>
     </div>
     <v-spacer />
-    <div class="w-100">
+    <div class="w-100 primario">
       <v-row class="galery">
         <v-col v-for="n in 20" :key="n" md="2">
-          <v-card v-ripple class="item" outlined @click="redirigir()">
+          <v-card
+            v-ripple
+            class="item borde"
+            color="primario"
+            outlined
+            @click="redirigir()"
+          >
             <img
               class="test"
               src="https://eso-hub.com/storage/icons/gear_breton_heavy_head_d.webp"
               alt=""
             />
-            <h5 class="centro">"Tipo"</h5>
-            <h4 class="centro">"Nombre"</h4>
+            <h5 class="centro texto1--text">"Tipo"</h5>
+            <h4 class="centro texto1--text">"Nombre"</h4>
           </v-card>
         </v-col>
       </v-row>
@@ -157,5 +168,13 @@
 }
 .centro {
   text-align: center;
+}
+.borde {
+  border: 1px solid white !important;
+  border-color: #a68f7b !important;
+}
+.primario {
+  border: 1px solid white !important;
+  border-color: #a68f7b !important;
 }
 </style>
