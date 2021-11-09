9e682a40523c0150e84592f13b207391a26798e1
Escarlet <sakutwf@gmail.com>
2021-11-09 19:39:12 -0300

    corrección de errores

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -5,22 +5,6 @@
       <v-btn text> Familias </v-btn>
       <v-btn text> Personajes </v-btn>
       <v-btn text> Items </v-btn>
-      <div>
-        <v-autocomplete
-          v-model="model"
-          :items="items"
-          :loading="isLoading"
-          :search-input.sync="search"
-          chips
-          clearable
-          hide-details
-          hide-selected
-          item-text="name"
-          item-value="symbol"
-          label="Búsqueda..."
-        >
-        </v-autocomplete>
-      </div>
     </v-row>
   </v-toolbar>
 </template>
