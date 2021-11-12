4f394e5f3c80428f214f09ca1c299a2fda559634
Mrldmz <leopoldo.munoz.m@gmail.com>
2021-11-12 00:24:16 -0300

    declarados los props de Slot.vue

---
+++ client/components/personajes/Slot.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    casilla: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      default: '',
+    },
+    trait: {
+      type: Object,
+      default: null,
+    },
+    glyph: {
+      type: Object,
+      default: null,
+    },
+    arma: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
