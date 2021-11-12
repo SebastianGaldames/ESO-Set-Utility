52bee9e4fd75446d1a61c260ed6c43e7d67cadcf
Mrldmz <leopoldo.munoz.m@gmail.com>
2021-11-11 22:59:38 -0300

    creada page Personajes

---
+++ client/pages/personajes/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Page Personajes</div>
+</template>
+
+<script>
+export default {
+  // components: { componente1... },
+  //   async asyncData({ $axios }) {
+  //     const personajesResponse = await $axios.$get(
+  //       process.env.VUE_APP_SERVER_URL + '/Personaje/list'
+  //     )
+  //     return { personajes: personajesResponse }
+  //   },
+  data() {
+    return {
+      personajes: [],
+      items: [],
+      itemsInventario: [],
+      familias: [],
+      glyphs: [],
+      traits: [],
+    }
+  },
+}
+</script>
