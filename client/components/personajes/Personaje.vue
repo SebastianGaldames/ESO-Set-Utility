1661bad99f2fa4e48120afa73e7ab56036eca258
Tyrael1999 <jamch1999@gmail.com>
2021-11-13 14:01:13 -0300

    Base para mostrar personaje con sus slot de inventario y page para comparar distintos personajes

---
+++ client/components/personajes/Personaje.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1 class="secundario--text" style="text-align: center">{{ nombre }}</h1>
+    <v-item-group>
+      <v-container class="equipamiento secundario--text">
+        <p style="text-align: center">Equipamiento</p>
+        <v-container>
+          <v-row align="center" justify="center">
+            <v-col v-for="n in 7" :key="n" md="3">
+              <v-card outlined>slot{{ n }}</v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
+      <v-container class="accesorios secundario--text">
+        <p style="text-align: center">Accesorios</p>
+        <v-container>
+          <v-row align="center" justify="center">
+            <v-col v-for="n in 3" :key="n" md="3">
+              <v-card outlined>slot{{ n }}</v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
+      <v-container class="armas secundario--text">
+        <p style="text-align: center">Armas</p>
+        <v-container>
+          <v-row align="center" justify="center">
+            <v-col v-for="n in 3" :key="n" md="3">
+              <v-card outlined>slot{{ n }}</v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
+    </v-item-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: 'Pepe',
+    }
+  },
+}
+</script>
---
+++ client/pages/compararpersonajes.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="primario" style="height: 100%">
+    <v-item-group>
+      <v-container>
+        <v-row>
+          <v-item class="primario" style="overflow: hidden">
+            <v-card width="33%">
+              <v-autocomplete
+                :items="personajes"
+                placeholder="Selecciona un personaje"
+                outlined
+                background-color="acentuado2"
+                color="acentuado1"
+              ></v-autocomplete>
+              <personaje class="bordes"></personaje>
+            </v-card>
+          </v-item>
+          <v-item class="primario">
+            <v-card class="bordes secundario--text" width="33%">stats</v-card>
+          </v-item>
+          <v-item class="primario">
+            <v-card width="33%">
+              <v-autocomplete
+                :items="personajes"
+                placeholder="Selecciona un personaje de referencia"
+                outlined
+                background-color="acentuado2"
+                color="acentuado1"
+              ></v-autocomplete>
+              <personaje class="bordes"></personaje>
+            </v-card>
+          </v-item>
+        </v-row>
+      </v-container>
+    </v-item-group>
+  </div>
+</template>
+
+<style>
+.bordes {
+  border-width: 2px;
+  border-style: solid;
+  border-color: #a68f7b;
+}
+</style>
+<script>
+import Personaje from '~/components/personajes/Personaje.vue'
+export default {
+  components: { Personaje },
+  data() {
+    return {
+      personajes: ['Pepe', 'Jaime', 'Manuel'],
+    }
+  },
+}
+</script>
