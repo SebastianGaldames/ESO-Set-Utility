65a29697d1f86ff95331165ce5f9b81743e5fb4a
Tyrael1999 <jamch1999@gmail.com>
2021-11-16 19:19:49 -0300

    Impedir merge conflict

--- client/pages/personajes/index.vue
+++ client/pages/personajes/index.vue
@@ -1,10 +1,22 @@
 <!-- Page Personajes -->
 <template>
-  <div></div>
+  <div>
+    Page Personajes
+    <seleccion-personaje v-model="selectedPersonaje" :personajes="personajes">
+    </seleccion-personaje>
+    {{ selectedPersonaje }}
+    <personaje-inventario
+      :familias="familias"
+      :items="items"
+    ></personaje-inventario>
+  </div>
 </template>
 
 <script>
+import PersonajeInventario from '~/components/personajes/PersonajeInventario.vue'
+import SeleccionPersonaje from '~/components/personajes/SeleccionPersonaje.vue'
 export default {
+  components: { SeleccionPersonaje, PersonajeInventario },
   // components: { componente1... },
   async asyncData({ $axios }) {
     // arreglo de pruebas, antes de implementar la obtención de inventario
@@ -51,7 +63,25 @@ export default {
       familias: [],
       glyphs: [],
       traits: [],
+      selectedPersonaje: {},
     }
+  },
+  mounted() {
+    this.makeDummyData()
+    this.selectedPersonaje = this.personajes[0]
+  },
+  methods: {
+    makeDummyData() {
+      const p1 = { nombre: 'juanin', email: 'asd@asd.com' }
+      const p2 = { nombre: 'tulio', email: 'qwe@asd.com' }
+      const p3 = { nombre: 'calcetin con rombosman', email: 'zxc@asd.com' }
+      this.personajes.push(p1, p2, p3)
+      // const item1 = { nombre: 'item1', tipo: 'two handed back scratcher' }
+      // const item2 = { nombre: 'item2', tipo: 'light socks' }
+      // const item3 = { nombre: 'item3', tipo: 'heavy eyelashes' }
+      // const item4 = { nombre: 'item4', tipo: 'weed pipe' }
+      // this.items.push(item1, item2, item3, item4)
+    },
   },
 }
 </script>
