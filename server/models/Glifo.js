b90092f85dfba0b6377a7abc21d87fd874f60567
Joaquin S <joaq4699@hotmail.com>
2021-11-16 01:56:01 -0300

    Creado endpoint para crear un personaje desde un usuario. Eliminado Slot del modelo, ahora incluido en el modelo de Personaje.

--- server/models/Glifo.js
+++ server/models/Glifo.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const potenciaSchema = new Schema({
   nombre: { type: String }, //Ej: Trifling Glyph of Health
+  tipo: { type: String }, //Ej: Trifling
   calidades: { type: Object }, //Ej: {normal: [{type: 'health', value: '50'}, {type: 'stamina', value: '30'}], fine:...}
 })
 
@@ -10,6 +11,7 @@ const glifoSchema = new Schema({
   tipo: { type: String, default: 'valor no asignado' }, //Ej: Armadura
   efectoDescripcion: { type: String, default: 'valor no asignado' }, //Ej: Adds [x] Max Health
   potencias: { type: [potenciaSchema] },
+  imagen: { type: String, default: 'no-asignado' },
 })
 
 module.exports = model('Glifo', glifoSchema)
--- server/models/Trait.js
+++ server/models/Trait.js
@@ -6,6 +6,7 @@ const traitSchema = new Schema({
   efectos: { type: [String], default: ['valor no asignado'] },
   calidades: { type: Object }, //Ej: {normal: [{type: 'health', value: '50'}, {type: 'stamina', value: '30'}], fine:...}
   calidades2h: { type: Object }, //Ej: {normal: [{type: 'health', value: '50'}, {type: 'stamina', value: '30'}], fine:...}
+  imagen: { type: String, default: 'no-asignado' },
 })
 
 module.exports = model('Trait', traitSchema)
