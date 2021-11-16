2e06189e03ae70d8773acdab788a8443b66af2dc
Mecho-sys <nrojazamorano@hotmail.com>
2021-11-16 00:26:46 -0300

    Se elimino la paleta de colores del componente.

--- client/components/Glifos/gliphsComp.vue
+++ client/components/Glifos/gliphsComp.vue
@@ -3,23 +3,23 @@
     <div class="w-100 mx-auto">
       <h3>Lista de Glifos</h3>
     </div>
-    <div class="w-100 mx-auto primario">
+    <div class="w-100 mx-auto">
       <v-responsive class="overflow-y-auto" max-height="200">
         <v-row class="galery">
-          <v-col v-for="n in 5" :key="n" md="3">
-            <v-card v-ripple dark style="border: 2px solid #a68f7b" outlined>
+          <v-col v-for="n in 38" :key="n" md="3">
+            <v-card v-ripple outlined>
               <img
                 class="img"
                 src="https://elderscrollsonline.wiki.fextralife.com/file/Elder-Scrolls-Online/glyph_of_health-eso-wiki-guide.png"
                 alt=""
               />
-              <h5 class="centro texto1--text">armadura</h5>
+              <h5 class="centro">armadura</h5>
               <v-spacer />
-              <hr class="linea secundario" />
-              <h4 class="centro texto1--text">Glyph of Magicka</h4>
-              <hr class="linea secundario" />
+              <hr class="linea" />
+              <h4 class="centro">Glyph of Magicka</h4>
+              <hr class="linea" />
               <v-spacer />
-              <h5 class="centro texto1--text">aumenta en X la magia</h5>
+              <h5 class="centro">aumenta en X la magia</h5>
             </v-card>
           </v-col>
         </v-row>
