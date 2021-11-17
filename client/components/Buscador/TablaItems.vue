3de877d7de7022e10ad6db83a8b61d0db0b177e4
Diego Aguilera <diegoaguilera4@icloud.com>
2021-11-16 21:20:42 -0300

    Filtros tabla item y se añaden atributos nuevos al ingresar a familia

--- client/components/Buscador/TablaItems.vue
+++ client/components/Buscador/TablaItems.vue
@@ -23,23 +23,47 @@
               :items="ubicaciones"
               label="Ubicacion"
               solo
-              @change="selected = true"
+              @change="selected1 = true"
             ></v-select>
           </v-col>
           <v-col class="d-flex">
-            <v-select :items="estilo" label="Estilo" solo></v-select>
+            <v-select
+              v-model="selectedTipo"
+              :items="tipos"
+              label="Tipo"
+              solo
+              @change="selected3 = true"
+            ></v-select>
           </v-col>
           <v-col class="d-flex">
-            <v-select :items="tipo" label="Tipo" solo></v-select>
+            <v-select
+              v-model="selectedEstilo"
+              :items="estilos"
+              label="Estilo"
+              solo
+              @change="selected2 = true"
+            ></v-select>
           </v-col>
         </v-row>
       </div>
       <div>
         <v-btn
-          v-if="(selected, selectedUbi !== '')"
+          v-if="(selected1, selectedUbi !== '')"
           elevation="2"
           @click=";(selectedUbi = ''), (select = false)"
           >{{ selectedUbi }} x</v-btn
+        >
+        <v-btn
+          v-if="(selected3, selectedTipo !== '')"
+          elevation="2"
+          @click=";(selectedTipo = ''), (select3 = false)"
+          >{{ selectedTipo }} x</v-btn
+        >
+        <v-btn
+          v-if="(selected2, selectedEstilo !== '')"
+          elevation="2"
+          @click=";(selectedEstilo = ''), (select2 = false)"
+          >{{ selectedEstilo }} x</v-btn
         >
       </div>
       <p>Familias</p>
@@ -93,12 +117,17 @@ export default {
   },
   data() {
     return {
-      selected: false,
+      selected1: false,
+      selected2: false,
+      selected3: false,
       ubicaciones: [],
-      estilo: [],
+      tipos: [],
+      estilos: [],
       tipo: [],
       search: '',
       selectedUbi: '',
+      selectedTipo: '',
+      selectedEstilo: '',
       auxNombre: '',
       columnas: [
         { value: 'imagen', sortable: false, width: '1%' },
@@ -114,23 +143,99 @@ export default {
           width: '30%',
           text: 'Ubicacion',
         },
+        {
+          sortable: true,
+          value: 'tipo',
+          width: '30%',
+          text: 'Tipo',
+        },
+        {
+          sortable: true,
+          value: 'estilo',
+          width: '30%',
+          text: 'Estilo',
+        },
       ],
     }
   },
   computed: {
     computed_items() {
       const filtroUbi = this.selectedUbi
+      const filtroTipo = this.selectedTipo
+      const filtroEstilo = this.selectedEstilo
       const aux = this.auxNombre
       return this.families.filter(function (item) {
         let filtered = true
         if (filtered) {
+          // Empieza con filtro ubicacion
           if (filtroUbi) {
             filtered = item.nombre === aux
-            item.ubicacion.forEach((element) => {
-              filtered += element === filtroUbi
-            })
+            // console.log(filtroUbi)
+            // Cuando hay ubicacion y tipo
+            if (filtroTipo) {
+              item.ubicacion.forEach((element) => {
+                if (element === filtroUbi) {
+                  if (item.tipo === filtroTipo) {
+                    // console.log(item.nombre)
+                    // Cuando hay ubicacion, tipo y estilo
+                    if (filtroEstilo) {
+                      if (item.estilo === filtroEstilo) {
+                        filtered += item
+                      }
+                    } else {
+                      // Cuando hay ubicacion y tipo solamente
+                      filtered += item
+                    }
+                  }
+                }
+              })
+              // Cuando hay ubicacion y estilo
+            } else if (filtroEstilo) {
+              item.ubicacion.forEach((element) => {
+                if (element === filtroUbi) {
+                  if (item.estilo === filtroEstilo) {
+                    // console.log(item.nombre)
+                    // Cuando hay ubicaicon, estilo y tipo
+                    if (filtroTipo) {
+                      if (item.tipo === filtroTipo) {
+                        filtered += item
+                      }
+                    } else {
+                      // Cuando hay ubicacion y estilo
+                      filtered += item
+                    }
+                  }
+                }
+              })
+            } else {
+              // Cuando hay solo ubicacion
+              item.ubicacion.forEach((element) => {
+                filtered += element === filtroUbi
+              })
+            }
+          } else if (filtroTipo) {
+            // Cuando es tipo
+            // console.log(filtroTipo)
+            filtered = item.nombre === aux
+            if (item.tipo === filtroTipo) {
+              // Cuando es tipo y estilo
+              if (filtroEstilo) {
+                if (item.estilo === filtroEstilo) {
+                  filtered += item
+                }
+              } else {
+                // Cuando es tipo
+                filtered += item.tipo === filtroTipo
+              }
+            }
+          } else if (filtroEstilo) {
+            // Cuando es estilo
+            // console.log(filtroEstilo)
+            filtered = item.nombre === aux
+            filtered += item.estilo === filtroEstilo
           } else {
             // cuando no hay filtros
+            // console.log('entree xdd')
             filtered = item
           }
         }
@@ -140,6 +245,8 @@ export default {
   },
   created() {
     this.transformarUbis()
+    this.transformarTipos()
+    this.transformarEstilos()
   },
   methods: {
     familiaToJson(item) {
@@ -150,6 +257,18 @@ export default {
       for (let i = 0; i < this.families.length; i++) {
         for (let j = 0; j < this.families[i].ubicacion.length; j++) {
           this.ubicaciones.push(this.families[i].ubicacion[j])
+        }
+      }
+    },
+    transformarTipos() {
+      for (let i = 0; i < this.families.length; i++) {
+        this.tipos.push(this.families[i].tipo)
+      }
+    },
+    transformarEstilos() {
+      for (let i = 0; i < this.families.length; i++) {
+        if (this.families[i].estilo !== '') {
+          this.estilos.push(this.families[i].estilo)
         }
       }
     },
