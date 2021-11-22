8d8b94f084cfc7d8d8e94921a8736f48f074737b
Tyrael1999 <jamch1999@gmail.com>
2021-11-22 01:27:01 -0300

    prevenir merge conf 2

---
+++ client/components/personajes/Slot.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div>
+      <v-img
+        class="mx-1"
+        :height="defaultItemHeight"
+        :src="itemImage"
+        :width="defaultItemWidth"
+        @click="test"
+      ></v-img>
+      <div class="d-flex py-2">
+        <v-img
+          :max-height="defaultGlyphTraitHeight"
+          :src="traitImage"
+          :max-width="defaultGlyphTraitWidth"
+          @click="test"
+        ></v-img>
+        <v-img
+          class="mx-2"
+          :max-height="defaultGlyphTraitHeight"
+          :src="glyphImage"
+          :max-width="defaultGlyphTraitWidth"
+          @click="test"
+        ></v-img>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    slotProp: {
+      type: Object,
+      required: true,
+    },
+    agregarItem: {
+      type: Boolean,
+      default: false,
+    },
+    agregarGlyph: {
+      type: Boolean,
+      default: false,
+    },
+    agregarTrait: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      defaultItemHeight: '50',
+      defaultItemWidth: '50',
+      defaultGlyphTraitHeight: '25',
+      defaultGlyphTraitWidth: '25',
+    }
+  },
+  computed: {
+    itemImage() {
+      if (!(this.slotProp.item === undefined)) {
+        return this.slotProp.item.imagen
+      }
+      return ''
+    },
+    glyphImage() {
+      if (!(this.slotProp.glyph === undefined)) {
+        return this.slotProp.item.imagen
+      }
+      return ''
+    },
+    traitImage() {
+      if (!(this.slotProp.trait === undefined)) {
+        return this.slotProp.item.imagen
+      }
+      return ''
+    },
+  },
+  methods: {
+    test(event) {
+      console.log('evento capturado')
+    },
+  },
+}
+</script>
