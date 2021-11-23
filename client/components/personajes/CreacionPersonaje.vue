dfa67ad8fd44c66aec83504517645c3565fc18da
Escarlet <sakutwf@gmail.com>
2021-11-23 16:12:48 -0300

    Agregada validación agregar personaje (nombre con mínimo 2 caracteres)

--- client/components/personajes/CreacionPersonaje.vue
+++ client/components/personajes/CreacionPersonaje.vue
@@ -1,34 +1,41 @@
 <template>
-  <v-card>
-    <v-col cols="12">
-      <v-card-title primary-title> Agregar Personaje </v-card-title>
-      <v-text-field
-        v-model="character.characterName"
-        label="Nombre del personaje"
-        :rules="rules"
-        hide-details="auto"
-      ></v-text-field>
-      <v-textarea
-        v-model="character.characterDescription"
-        label="Descripción del personaje"
-        auto-grow
-        row="2"
-        row-height="20"
-      ></v-textarea>
-    </v-col>
-    <v-card-actions class="px-5 pb-4">
-      <v-flex text-center>
-        <v-btn color="acentuado2" class="mx-10" @click="addCharacter"
-          >Agregar Personaje</v-btn
-        >
-        <v-btn outlined blame @click="closeDialogEvent">Cancelar</v-btn>
-      </v-flex>
-    </v-card-actions>
-  </v-card>
+  <v-form ref="form" v-model="valid">
+    <v-card>
+      <v-col cols="12">
+        <v-card-title primary-title> Agregar Personaje </v-card-title>
+        <v-text-field
+          v-model="character.characterName"
+          label="Nombre del personaje"
+          :rules="rules"
+          hide-details="auto"
+        ></v-text-field>
+        <v-textarea
+          v-model="character.characterDescription"
+          label="Descripción del personaje"
+          auto-grow
+          row="2"
+          row-height="20"
+        ></v-textarea>
+      </v-col>
+      <v-card-actions class="px-5 pb-4">
+        <v-flex text-center>
+          <v-btn
+            color="acentuado2"
+            class="mx-10"
+            :disabled="!valid"
+            @click="addCharacter"
+            >Agregar Personaje</v-btn
+          >
+          <v-btn outlined blame @click="closeDialogEvent">Cancelar</v-btn>
+        </v-flex>
+      </v-card-actions>
+    </v-card>
+  </v-form>
 </template>
 <script>
 export default {
   data: () => ({
+    valid: true,
     rules: [
       (value) => !!value || 'Campo obligatorio',
       (value) => (value && value.length >= 2) || 'Min 2 caracteres',
@@ -37,6 +44,7 @@ export default {
   }),
   methods: {
     addCharacter() {
+      this.$refs.form.validate()
       this.$emit('newCharacterEvent', this.character)
     },
     closeDialogEvent() {
