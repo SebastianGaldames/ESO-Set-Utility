c3b5457dc7d56d9a51a145378aa7f32cd249a735
Mecho-sys <nrojazamorano@hotmail.com>
2021-12-04 22:23:19 -0300

    Se agrego una page de About Us

---
+++ client/pages/AboutUs/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <div class="w-100 textCentro">
+      <v-row>
+        <v-col md="2"></v-col>
+        <v-col class="" md="8">
+          <h1>¿Quienes Somos?</h1>
+          <hr class="linea" />
+        </v-col>
+        <v-col md="2"></v-col>
+      </v-row>
+      <v-row>
+        <v-col md="2"></v-col>
+        <v-col md="8">
+          <h3>Sebastian Galdames</h3>
+          <h3>Diego Aguilera</h3>
+          <h3>Christian Lizama</h3>
+          <h3>Manuel Morales</h3>
+          <h3>Joaquin Moya</h3>
+          <h3>Leopoldo Muñoz</h3>
+          <h3>Francisco Quevedo</h3>
+          <h3>Joaquin Saavedra</h3>
+          <h3>NicolasRojas</h3>
+          <h3>Escarlet Tapia</h3>
+          <v-spacer />
+          <hr class="linea" />
+        </v-col>
+        <v-col md="2"></v-col>
+      </v-row>
+      <v-row>
+        <v-col md="2"></v-col>
+        <v-col md="8">
+          <h3>Agradecemos que usen esta página</h3>
+        </v-col>
+        <v-col md="2"></v-col>
+      </v-row>
+    </div>
+  </section>
+</template>
+
+<style>
+.w-100 {
+  width: 100%;
+  align-content: center;
+}
+.textCentro {
+  text-align: center;
+  padding: 5px 10px;
+}
+.linea {
+  width: 100%;
+  align-content: center;
+}
+</style>
