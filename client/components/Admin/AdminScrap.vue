7816b300b203feb2d64aa0e34a69f389b316571c
Francisco-q <quevedof98@gmail.com>
2021-12-07 20:51:01 -0300

    admin boton solo front v1

---
+++ client/components/Admin/AdminScrap.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">BIENVENIDO ADMINISTRADOR</div>
+        <v-list-item-title class="text-h5 mb-1">
+          Pulsa boton para scrap
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-progress-linear
+      v-model="power"
+      color="amber"
+      height="25"
+    ></v-progress-linear>
+    <v-card-actions>
+      <v-btn outlined rounded text class="center" @click="scrapPage">
+        Pulsa
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      power: 0,
+    }
+  },
+  methods: {
+    scrapPage() {
+      let i = 0
+      for (this.power; i < 100; i++) {
+        this.power += i
+      }
+    },
+  },
+}
+</script>
---
+++ client/pages/AdminScrap/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <admin-scrap></admin-scrap>
+  </div>
+</template>
+
+<script>
+import AdminScrap from '~/components/Admin/AdminScrap.vue'
+export default {
+  components: { AdminScrap },
+
+  data() {},
+}
+</script>
