bc69b8702f90fd31b459635600a029ee9701be27
Francisco-q <quevedof98@gmail.com>
2021-12-07 21:54:44 -0300

    vista del scrap oculta

--- client/pages/AdminScrap/index.vue
+++ client/pages/AdminScrap/index.vue
@@ -9,6 +9,20 @@ import AdminScrap from '~/components/Admin/AdminScrap.vue'
 export default {
   components: { AdminScrap },
 
-  data() {},
+  data() {
+    return {
+      usuarioLogeado: false,
+      usuario: null,
+    }
+  },
+  beforeCreate() {
+    const usuario = this.$store.state.usuario
+    if (usuario === 'administrador') {
+      this.usuario = usuario
+      this.usuarioLogeado = true
+    } else {
+      this.$router.push({ name: 'index' })
+    }
+  },
 }
 </script>
