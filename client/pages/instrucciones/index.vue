c8a5bd52b7a16305ec3edc59e5c9d4b4b39e2c0c
Escarlet <sakutwf@gmail.com>
2021-12-08 04:30:18 -0300

    Vista Instrucciones - sección Familias terminada

--- client/pages/instrucciones/index.vue
+++ client/pages/instrucciones/index.vue
@@ -84,6 +84,13 @@
                         <h4 class="mt-16" style="text-align: justify">
                           <!-- https://i.ibb.co/yXbdRtR/passrecovery2.png
 src="https://i.ibb.co/18d3jTw/passrecovery3.png"
+ barra superior
+ barra inferior
+ pantalla familia
+ 
+ ejemplo busqueda
+ contenido de familia
+
                           -->
                           Para Recuperar contraseña deberás seguir una serie de
                           pasos que se han dispuesto con el fin de dar una mayor
@@ -137,7 +144,105 @@ src="https://i.ibb.co/18d3jTw/passrecovery3.png"
                 <v-expansion-panel-header>
                   <h2>Sección Familias</h2></v-expansion-panel-header
                 >
-                <v-expansion-panel-content> asd </v-expansion-panel-content>
+                <v-expansion-panel-content>
+                  <div class="mt-2 mb-5">
+                    <v-row justify="center">
+                      <v-col class="pl-10" cols="8">
+                        <h4 class="mt-16" style="text-align: justify">
+                          La sección de Familias se ha creado con el fin de
+                          entregar toda la información necesaria sobre los items
+                          pertenecientes a cada una de ellas.. si quieres saber
+                          los detalles de algún item en específico este es tu
+                          lugar. Para acceder a esta sección debes dirigirte a
+                          la barra de navegación ubicada en la parte superior
+                          derecha de la pantalla y localizar "Familias"
+                          <v-img
+                            class="mt-4"
+                            aspect-ratio="15"
+                            contain
+                            src="https://i.ibb.co/NYTncYG/navbar.png"
+                          ></v-img>
+                          Al hacer click se te redirigirá a una vista como la
+                          que se muestra a continuación:
+                          <v-img
+                            aspect-ratio="1"
+                            contain
+                            src="https://i.ibb.co/KsD5Vgy/familias1.png"
+                          ></v-img>
+
+                          En ella se encuentra el listado total de los items
+                          utilizables en el juego, clasificados por nombre,
+                          ubicacion, tipo y peso, para navegar por esta tabla en
+                          la esquina inferior derecha te encontrarás con una
+                          barra que muestra cuantos items son mostrados por
+                          página, cuantas páginas hay con las coincidencias y
+                          las flechas para moverte por cada una, hacia la
+                          izquierda o hacia la derecha:
+
+                          <v-img
+                            class="mt-6"
+                            aspect-ratio="13"
+                            contain
+                            src="https://i.ibb.co/PjkRmFZ/familias-inferior.png"
+                          ></v-img>
+                          En ella te encontrarás con una barra de búsqueda desde
+                          la cual puedes escribir palabras que coincidan con el
+                          nombre, ubicacion, tipo o peso del item que desees
+                          revisar:
+
+                          <v-img
+                            aspect-ratio="6"
+                            contain
+                            src="https://i.ibb.co/fGsZmQ9/familia-superior.png"
+                          ></v-img>
+                          Un ejemplo de esto se muestra a continuación:
+                          <v-img
+                            class="mt-6"
+                            aspect-ratio="2"
+                            contain
+                            src="https://i.ibb.co/tcxjQYx/Familias5.png"
+                          ></v-img>
+                          También existe otra manera de ubicar el item que
+                          desees revisar, esto se hace utilizando los filtros
+                          con opciones que se encuentran bajo la barra de
+                          búsqueda, de izquierda a derecha puedes ver los
+                          filtros de: Búsqueda:
+                          <v-img
+                            aspect-ratio="2"
+                            contain
+                            src="https://i.ibb.co/hWD81cZ/familias2.png"
+                          ></v-img>
+                          Tipo:
+                          <v-img
+                            aspect-ratio="2"
+                            contain
+                            src="https://i.ibb.co/DDNmrnb/familias3.png"
+                          ></v-img>
+
+                          Peso:
+
+                          <v-img
+                            aspect-ratio="3"
+                            contain
+                            src="https://i.ibb.co/gJzTy82/Familias4.png"
+                          ></v-img>
+
+                          Una vez se han puesto todos los filtros o ya has
+                          identificado en pantalla el item del que deseas
+                          conocer los detalles, solo debes hacer click sobre el
+                          ícono del item y te enviara a la vista con los
+                          detalles:
+                          <v-img
+                            class="mt-6"
+                            aspect-ratio="1.7"
+                            contain
+                            src="https://i.ibb.co/W6j9MQ9/Familias-Contenido.png"
+                          ></v-img>
+                        </h4>
+                      </v-col>
+                    </v-row>
+                  </div>
+                </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
           </v-card-actions>
